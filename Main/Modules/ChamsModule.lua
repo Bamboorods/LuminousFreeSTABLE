8237a19cf24e939bdf5ad842064cc8d9b3c8b764
Bamboorods <81838210+Bamboorods@users.noreply.github.com>
2024-08-05 10:23:25 -0400

    Update ChamsModule.lua

--- Main/Modules/ChamsModule.lua
+++ Main/Modules/ChamsModule.lua
@@ -1,20 +1,23 @@
-local ChamsModule = {}
 warn("ChamsModuleLoaded")
+
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
+local ChamsModule = {}
 ChamsModule.features = {
     chams = {
         enabled = false,
         teamcheck = false,
         color = {
-            fill = Color3.fromRGB(0, 7, 167),
-            outline = Color3.fromRGB(0, 18, 64)
+            fill = Color3.new(0, 7, 167),
+            outline = Color3.new(0, 18, 64)
         },
-        transparency = {
-            fill = 0.74,
-            outline = 0.38
+        fill = {
+            transparency =  0.74
+        },
+        Outline = {
+            transparency = 0.38
         }
     }
 }
@@ -27,29 +30,32 @@ local function is_ally(player)
     return player.Team == LocalPlayer.Team
 end
 
-local function update_chams()
+function update_chams()
     for _, player in pairs(get_players()) do
-        local highlight = player:FindFirstChildWhichIsA("Highlight")
-        if ChamsModule.features.chams.enabled then
-            local ally = is_ally(player)
-            local should_highlight = not ChamsModule.features.chams.teamcheck or (ChamsModule.features.chams.teamcheck and not ally)
-            
-            if should_highlight then
-                if not highlight then
-                    highlight = Instance.new("Highlight", player)
+        if player.Character then
+            local highlight = player.Character:FindFirstChildWhichIsA("Highlight")
+            if ChamsModule.features.chams.enabled then
+                local ally = is_ally(player)
+                local should_highlight = not ChamsModule.features.chams.teamcheck or (ChamsModule.features.chams.teamcheck and not ally)
+
+                if should_highlight then
+                    if not highlight then
+                        highlight = Instance.new("Highlight")
+                        highlight.Parent = player.Character
+                    end
+                    highlight.FillColor = ChamsModule.features.chams.color.fill
+                    highlight.OutlineColor = ChamsModule.features.chams.color.outline
+                    highlight.FillTransparency = ChamsModule.features.chams.fill.transparency
+                    highlight.OutlineTransparency = ChamsModule.features.chams.outline.transparency
+                else
+                    if highlight then
+                        highlight:Destroy()
+                    end
                 end
-                highlight.FillColor = ChamsModule.features.chams.color.fill
-                highlight.OutlineColor = ChamsModule.features.chams.color.outline
-                highlight.FillTransparency = ChamsModule.features.chams.transparency.fill
-                highlight.OutlineTransparency = ChamsModule.features.chams.transparency.outline
             else
                 if highlight then
                     highlight:Destroy()
                 end
-            end
-        else
-            if highlight then
-                highlight:Destroy()
             end
         end
     end
@@ -84,5 +90,11 @@ function ChamsModule.setOutlineTransparency(value)
     ChamsModule.features.chams.transparency.outline = value
     update_chams()
 end
+
+RunService.RenderStepped:Connect(function()
+    if ChamsModule.features.chams.enabled then
+        update_chams()
+    end
+end)
 
 return ChamsModule
