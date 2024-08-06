2df83c7934a8618fa129f985a55dcf059b6d8a50
Bamboorods <81838210+Bamboorods@users.noreply.github.com>
2024-08-06 08:16:19 -0400

    Update ChamsModule.lua

--- Main/Modules/ChamsModule.lua
+++ Main/Modules/ChamsModule.lua
@@ -1,102 +1,99 @@
---// Services
-local Players_service = game:GetService("Players")
-local Workspace = game:GetService("Workspace")
-local StarterGui = game:GetService("StarterGui")
-local Teams = game:GetService("Teams")
+local ChamsModule = {}
+
 local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
-local player = Players_service.LocalPlayer or Players_service.LocalPlayer:wait();
-
---// Tables
-ChamsModule = {}
-ChamsModule.features = {
+local features = {
     chams = {
         enabled = false,
         teamcheck = false,
-        color = {
-            fill = Color3.fromRGB(0, 7, 167),
-            outline = Color3.fromRGB(0, 18, 64)
-        },
-        ctransparency = {
-            fill = 0.74,
-            outline = 0.38
-        }
+        color = {fill = Color3.fromRGB(0, 7, 167), outline = Color3.fromRGB(0, 18, 64)},
+        transparency = {fill = 0.74, outline = 0.38}
     }
 }
 
---// Functions
+
+local function onPlayerAdded(player)
+    player.CharacterAdded:Connect()
+end
+
 local function get_players()
-    return Players_service:GetPlayers() 
-end
-
-local LocalPlayer = Players.LocalPlayer or game.Players.LocalPlayer
-    return player.Team == LocalPlayer.Team
-end
-
-function update_chams()
-    for _, player in pairs(get_players()) do
-        if player.Character or player.CharacterAdded:wait() then
-            local highlight = player.Character:FindFirstChildWhichIsA("Highlight")
-            if ChamsModule.features.chams.enabled then
-                local ally = is_ally(player)
-                local should_highlight = not ChamsModule.features.chams.teamcheck or (ChamsModule.features.chams.teamcheck and not ally)
-
-                if should_highlight then
-                    if not highlight then
-                        highlight = Instance.new("Highlight")
-                        highlight.Parent = player
-                        print("Highlight created for player:", player.Name)
-                    end
-                    highlight.FillColor = ChamsModule.features.chams.color.fill
-                    highlight.OutlineColor = ChamsModule.features.chams.color.outline
-                    highlight.FillTransparency = ChamsModule.features.chams.ctransparency.fill
-                    highlight.OutlineTransparency = ChamsModule.features.chams.ctransparency.outline
-                else
-                    if highlight then
-                        highlight:Destroy()
-                        print("Highlight destroyed for player:", player.Name)
-                    end
-                end
-            else
-                if highlight then
-                    highlight:Destroy()
-                    print("Highlight destroyed for player:", player.Name)
-                end
-            end
-        else
-            print("Player character not found for player:", player.Name)
+    local entity_list = {}
+    for _, player in ipairs(Players:GetPlayers()) do
+        if player:IsA("Model") then
+            table.insert(entity_list, player)
         end
+    end
+    return entity_list
+end
+
+local function is_ally(player)
+    if not player then return false end
+
+    local helmet = player:FindFirstChildWhichIsA("Folder"):FindFirstChildOfClass("MeshPart")
+    if not helmet then return false end
+
+    return helmet.BrickColor == BrickColor.new("Black") and LocalPlayer.Team == "Phantoms" or LocalPlayer.Team == "Ghosts"
+end
+
+local function onCharacterLoad(character)
+    local highlight = Instance.new("Highlight");
+
+    highlight.FillColor = features.chams.color.fill
+
+    highlight.OutlineColor = features.chams.color.outline
+
+    highlight.FillTransparency = features.chams.transparency.fill
+
+    highlight.OutlineTransparency = features.chams.transparency.outline
+
+    highlight.Parent = character;
+end
+
+
+
+local function updateChams()
+    for _, player in ipairs(get_players()) do
+        local highlight = player:FindFirstChild("Highlight")
+
+        if not highlight then
+            return;
+        end
+
+        if not features.chams.enabled then
+            highlight.Visible = false;
+            return;
+        end
+
+        local ally = is_ally(player)
+        local shouldHighlight = not features.chams.teamcheck or (features.chams.teamcheck and not ally)
+            
+        if not shouldHighlight then
+            highlight.Visible = false;
+            return
+        end
+
+        highlight.Visible = true;
     end
 end
 
 function ChamsModule.setChamsEnabled(value)
-    ChamsModule.features.chams.enabled = value
+    features.chams.enabled = value
+    updateChams()
 end
 
-function ChamsModule.setTeamCheckEnabled(value)
-    ChamsModule.features.chams.teamcheck = value
+function ChamsModule.isChamsEnabled()
+    return features.chams.enabled
 end
 
-function ChamsModule.setFillColor(color)
-    ChamsModule.features.chams.color.fill = color
-end
-
-function ChamsModule.setOutlineColor(color)
-    ChamsModule.features.chams.color.outline = color
-end
-
-function ChamsModule.setFillTransparency(value)
-    ChamsModule.features.chams.ctransparency.fill = value
-end
-
-function ChamsModule.setOutlineTransparency(value)
-    ChamsModule.features.chams.ctransparency.outline = value
-end
+Players.PlayerAdded:Connect(onPlayerAdded)
 
 RunService.RenderStepped:Connect(function()
-    if ChamsModule.features.chams.enabled then
-        update_chams()
+    if features.chams.enabled then
+        updateChams()
     end
 end)
-warn("ChamsModuleLoaded")
+
+
 return ChamsModule
